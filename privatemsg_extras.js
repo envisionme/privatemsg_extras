b262bb3ce33ac9fc199eb9fd3513f0b19456e924
Rui Ferreira <ferreira.rcb@gmail.com>
2012-10-15 09:08:08 +0000

    Comment out private message extras js

--- privatemsg_extras.js
+++ privatemsg_extras.js
@@ -1,7 +1,7 @@
 Drupal.behaviors.privatemsg_extras = function (context) {
-  $("#privatemsg-exras form").hide();
+  /*$("#privatemsg-exras form").hide();
 	$("#privatemsg-exras a.uibutton").click(function(e) {
     e.preventDefault();
     $("#privatemsg-exras form").toggle("fast");
-	});
+	});*/
 }
